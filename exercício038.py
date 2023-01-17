21bcbd0aaeceb5df72dce829bd4fe9e7ad00f12e
mfranklinpython <mfranklin.tec@gmail.com>
2023-01-17 10:26:39 -0300

    exercício 38

---
+++ exercício038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+if n1 > n2:
+    print('Primeiro número é MAIOR!')
+elif n2 > n1:
+    print('Segundo número é MAIOR!')
+else:
+    print('Os número são iguais')
